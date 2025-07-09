a97a9588d75d2b48c2aa49c0435dc08156cb09f5
JdaieLin <490844515@qq.com>
2025-07-09 13:28:09 +0800

    feat: github deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://pisugar.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'PiSugar', // Usually your GitHub org/user name.
+  projectName: 'pisugar-doc', // Usually your repo name.
+  deploymentBranch: 'main', // The branch that GitHub pages will deploy from.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

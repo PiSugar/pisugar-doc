84179aafafd4a2123988b4d3e38d39f62a0967e9
JdaieLin <490844515@qq.com>
2025-07-09 13:46:46 +0800

    feat: custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://pisugar.github.io',
+  url: 'https://doc.pisugar.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pisugar-doc/',
+  baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

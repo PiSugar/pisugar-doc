e90dfa4c6d311b10b149063fe9a6cd6c6cf7b3c1
JdaieLin <490844515@qq.com>
2025-07-12 09:40:53 +0800

    feat: meta tag

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,6 +30,16 @@ const config: Config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
+  headTags: [
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'algolia-site-verification',
+        content: '8A34CE678B7D7BDD',
+      },
+    },
+  ],
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
